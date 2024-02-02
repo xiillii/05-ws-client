b6bb5c477424b80d530f11884749fee876c0f931
Jose Alonso <mail@josealonso.dev>
2024-02-01 21:46:14 -0600

    Emit message to the websocket server

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,10 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
     <ul id='clients-ul'>
     </ul>
+
+    <form id='message-form'>
+      <input placeholder='message' id='message-input' />
+    </form>
   </div>
 `
 
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -12,7 +12,9 @@ const addListeners = (socket: Socket) => {
   const serverStatusLabel = document.querySelector('#server-status')
   const clientsUl = document.querySelector('#clients-ul')!
 
-  // TODO: #clients-ul
+  const messageForm = document.querySelector<HTMLFormElement>('#message-form')!
+  const messageInput =
+    document.querySelector<HTMLInputElement>('#message-input')!
 
   socket.on('connect', () => {
     serverStatusLabel!.innerHTML = 'connected'
@@ -34,5 +36,17 @@ const addListeners = (socket: Socket) => {
     })
 
     clientsUl.innerHTML = clientsHtml
+  })
+
+  messageForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    if (messageInput.value.trim().length <= 0) return
+
+    socket.emit('message-from-client', {
+      id: 'ME!!!',
+      message: messageInput.value,
+    })
+
+    messageInput.value = ''
   })
 }
