8dba181a3afee8e91e1ac9e85ba3b358b9ed9c5a
Jose Alonso <mail@josealonso.dev>
2024-02-02 19:09:44 -0600

    Fix when you reconnect the user
    You can send messages again, in reconnections

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,5 +1,7 @@
 import { Manager, Socket } from 'socket.io-client'
 
+let socket: Socket
+
 export const connectToServer = (token: string) => {
   const manager = new Manager('http://localhost:3003/socket.io/socket.io.js', {
     extraHeaders: {
@@ -8,12 +10,13 @@ export const connectToServer = (token: string) => {
     },
   })
 
-  const socket = manager.socket('/')
+  socket?.removeAllListeners()
+  socket = manager.socket('/')
 
-  addListeners(socket)
+  addListeners()
 }
 
-const addListeners = (socket: Socket) => {
+const addListeners = () => {
   const serverStatusLabel = document.querySelector('#server-status')
   const clientsUl = document.querySelector('#clients-ul')!
 
